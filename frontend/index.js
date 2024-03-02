4897d9851bb94e86e65444058a54778be92380e8
Daniel Simboli <das817@lehigh.edu>
2024-03-02 15:42:32 -0500

    center map at user location

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,40 @@
 let map;
 
+function getUserLocation() {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject(new Error("Geolocation is not supported by your browser."));
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          resolve({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        () => {
+          reject(new Error("Unable to retrieve your location."));
+        }
+      );
+    }
+  });
+}
+
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
+  let mapCenter;
+
+  try {
+    mapCenter = await getUserLocation();
+  } catch(error) {
+    mapCenter = { lat: -34.397, lng: 150.644 }; // documentation default
+    console.error(error.message);
+  }
+
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: mapCenter,
+    zoom: 15,
   });
 }
 
